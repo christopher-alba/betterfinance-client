5abee83472c14cba80f482ee3ec9e470522975ec
Christopher Alba <christopher.alba@soeidental.com>
2022-01-02 08:48:34 +1300

    move hero component into its own actual component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,8 @@
 import React from "react";
-import { Container } from "../../components/Container";
-import {
-  HeroDiv,
-  BetterFinanceHeader,
-  BetterMoneyHeader,
-  BetterFutureHeader,
-  BetterLifeHeader,
-  Logo,
-  HeroText,
-} from "./styled";
+import Hero from "./Hero";
 
 const Home = () => {
-  return (
-    <HeroDiv>
-      <Container
-        flex
-        column
-        alignItems="center"
-        justifyContent="center"
-        textAlign="center"
-      >
-        <Logo />
-        <BetterLifeHeader>Better Life</BetterLifeHeader>
-        <BetterFutureHeader>Better Future</BetterFutureHeader>
-        <BetterMoneyHeader>Better Money</BetterMoneyHeader>
-        <BetterFinanceHeader>Better Finance</BetterFinanceHeader>
-        <hr style={{ border: "1px solid white", width: "50%", margin: 30 }} />
-        <HeroText>Helping you manage your finances is our top priority</HeroText>
-      </Container>
-    </HeroDiv>
-  );
+  return <Hero />;
 };
 
 export default Home;
