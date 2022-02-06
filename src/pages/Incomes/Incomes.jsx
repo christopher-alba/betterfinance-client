08e458a3c412b017e316caf6e4b6f04619515dc2
Christopher Alba <christopher.alba@soeidental.com>
2022-02-06 22:17:43 +1300

    visualize incomes on a graph

--- src/pages/Incomes/Incomes.jsx
+++ src/pages/Incomes/Incomes.jsx
@@ -1,27 +1,189 @@
 import { useMutation, useQuery } from "@apollo/client";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "styled-components";
 import Loading from "../../components/Loading";
 import { Container } from "../../components/Container";
 import { Title } from "../../components/Title";
 import { INCOMES } from "../../graphql/queries";
-import { MainDiv } from "./styled";
-import { Button, Icon, Table } from "semantic-ui-react";
+import { ChartDiv, MainDiv } from "./styled";
+import { Button, Icon, Select, Table } from "semantic-ui-react";
 import { DELETEINCOME } from "../../graphql/mutations";
 import CreateIncomeModal from "./CreateIncomeModal";
 import IncomeTableRow from "./IncomeTableRow";
 import UpdateIncomeModal from "./UpdateIncomeModal/UpdateIncomeModal";
-
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import useWindowWidth from "../../hooks/useWindowWidth";
+const frequencyOptions = [
+  { key: "Daily", value: "Daily", text: "Daily" },
+  { key: "Weekly", value: "Weekly", text: "Weekly" },
+  { key: "Monthly", value: "Monthly", text: "Monthly" },
+  { key: "Yearly", value: "Yearly", text: "Yearly" },
+];
 const Incomes = () => {
   const themeContext = useContext(ThemeContext);
   const [selectedIncome, setSelectedIncome] = useState(undefined);
+  const [selectedFrequency, setSelectedFrequency] = useState("Yearly");
+  const [chartData, setChartData] = useState(undefined);
   const profileID = localStorage.getItem("profileID");
   const { loading, data } = useQuery(INCOMES, {
     variables: {
       profileID,
     },
   });
+  const windowWidth = useWindowWidth();
   const [deleteIncome, { loading: deletingIncome }] = useMutation(DELETEINCOME);
+  useEffect(() => {
+    setChartData(
+      data?.getAllUserIncomes
+        .filter((income) => income.active)
+        .map((income) => {
+          var newIncomeObj;
+          switch (selectedFrequency) {
+            case "Daily":
+              switch (income.frequency) {
+                case "Daily":
+                  return {
+                    ...income,
+                    amount: parseFloat(income.amount).toFixed(2),
+                  };
+                case "Weekly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) / 7).toFixed(2),
+                  };
+                  break;
+                case "Monthly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) / 30.5).toFixed(2),
+                  };
+                  break;
+                case "Yearly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) / 365).toFixed(2),
+                  };
+                  break;
+                default:
+                  break;
+              }
+              break;
+            case "Weekly":
+              switch (income.frequency) {
+                case "Daily":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) * 7).toFixed(2),
+                  };
+                  break;
+                case "Weekly":
+                  return {
+                    ...income,
+                    amount: parseFloat(income.amount).toFixed(2),
+                  };
+                case "Monthly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) / 4.34524).toFixed(2),
+                  };
+                  break;
+                case "Yearly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) / 52).toFixed(2),
+                  };
+                  break;
+                default:
+                  break;
+              }
+              break;
+            case "Monthly":
+              switch (income.frequency) {
+                case "Daily":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) * 30.5).toFixed(2),
+                  };
+                  break;
+                case "Weekly":
+                  return {
+                    ...income,
+                    amount: (parseFloat(income.amount) * 4.34524).toFixed(2),
+                  };
+                case "Monthly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: parseFloat(income.amount).toFixed(2),
+                  };
+                  break;
+                case "Yearly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) / 12).toFixed(2),
+                  };
+                  break;
+                default:
+                  break;
+              }
+              break;
+            case "Yearly":
+              switch (income.frequency) {
+                case "Daily":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) * 365).toFixed(2),
+                  };
+                  break;
+                case "Weekly":
+                  return {
+                    ...income,
+                    amount: (parseFloat(income.amount) * 52).toFixed(2),
+                  };
+                case "Monthly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: (parseFloat(income.amount) * 12).toFixed(2),
+                  };
+                  break;
+                case "Yearly":
+                  newIncomeObj = {
+                    ...income,
+                    amount: parseFloat(income.amount).toFixed(2),
+                  };
+                  break;
+                default:
+                  break;
+              }
+              break;
+            default:
+              break;
+          }
+          return newIncomeObj;
+        })
+    );
+  }, [data, selectedFrequency]);
+
+  const findLargestAmount = (chartData) => {
+    let largestNumber = 0;
+    console.log(chartData);
+    for (let i = 0; i < chartData?.length; i++) {
+      if (largestNumber < parseFloat(chartData[i].amount)) {
+        largestNumber = chartData[i].amount;
+      }
+    }
+    console.log(largestNumber);
+    return largestNumber * 1.3;
+  };
+
   if (loading) {
     return <Loading message="Loading Your Incomes..." />;
   }
@@ -98,6 +260,40 @@ const Incomes = () => {
             </Table.Row>
           </Table.Footer>
         </Table>
+        <ChartDiv>
+          <h1 style={{ color: "black" }}>
+            Your Incomes Visualised (All converted to {selectedFrequency}{" "}
+            income)
+          </h1>
+          <Select
+            options={frequencyOptions}
+            defaultValue={selectedFrequency}
+            placeholder="Frequency"
+            onChange={(evt, data) => {
+              setSelectedFrequency(data.value);
+            }}
+          />
+          <ResponsiveContainer width="100%" height={500}>
+            <BarChart
+              layout="vertical"
+              data={chartData}
+              margin={{
+                top: 10,
+                right: 0,
+                bottom: 10,
+                left: windowWidth < 500 ? 50 : 100,
+              }}
+              barSize={50}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <YAxis dataKey="name" type="category" />
+              <XAxis type="number" domain={[0, findLargestAmount(chartData)]} />
+              <Tooltip contentStyle={{color:"black"}} />
+              <Legend />
+              <Bar dataKey="amount" fill="#02b191" scale />
+            </BarChart>
+          </ResponsiveContainer>
+        </ChartDiv>
       </Container>
     </MainDiv>
   );
--- src/pages/Incomes/styled.js
+++ src/pages/Incomes/styled.js
@@ -4,3 +4,10 @@ export const MainDiv = styled("div")`
   padding-top: 200px;
   padding-bottom: 100px;
 `;
+
+export const ChartDiv = styled("div")`
+  background: ${({ theme }) =>
+    theme.name === "light" ? "rgba(0, 0, 0, 0.05)" : "lightgray"};
+  padding: 30px;
+  border-radius: 5px;
+`;
